b10a5e7f460ded1b1b9614ef82a10348e0930c9c
siri <lolipopnapalochke2281337@gmail.com>
2021-11-19 19:39:17 +0500

    first release

---
+++ main.py
@@ -0,0 +1,45 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+import sys
+from random import randint
+from PyQt5 import uic
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.initUi()
+
+    def initUi(self):
+        self.setGeometry(400, 400, 400, 400)
+        self.draw_now = False
+
+        self.circles_button.clicked.connect(self.redraw)
+
+    def redraw(self):
+        self.draw_now = True
+        self.repaint()
+
+    def paintEvent(self, e):
+        if self.draw_now:
+            painter = QPainter()
+            painter.begin(self)
+            self.draw_circles(painter)
+            self.draw_now = False
+            painter.end()
+
+    def draw_circles(self, painter):
+        x, y = self.size().width(), self.size().height()
+        painter.setBrush(QColor('yellow'))
+        for _ in range(randint(5, 15)):
+            new_x, new_y = randint(0, x - 30), randint(0, y - 30)
+            size = randint(1, 30)
+            painter.drawEllipse(new_x, new_y, size, size)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    app.exec()
